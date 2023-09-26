75b0cecf835eb51ec59ec2df92cb027a1a5bf0bc
Everson Alves <pepo100@outlook.com>
2023-09-25 22:50:28 -0300

    Starting HTML templates for commands generator app

--- commands_generator/views.py
+++ commands_generator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     """Render HTML homepage"""
-    return render(request, 'homepage.html')
+    return render(request, 'index.html')
---
+++ maintence_manager/static/resources/clientSideMaintence.js
@@ -0,0 +1,7 @@
+async function getHost(){
+
+}
+
+async function getDevices(){
+
+}
